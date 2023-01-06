b2a82ab6dd531629930d5b6ef87a430676504c7a
Min-ki Park <browneyedsoul@yonsei.ac.kr>
2023-01-06 12:34:39 +0900

    Update powerup

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,6 +1,34 @@
 import { declareIndexPlugin, ReactRNPlugin } from "@remnote/plugin-sdk";
 
-async function onActivate(plugin: ReactRNPlugin) {}
+export const CALLOUT_POWERUP = "callout_powerup";
+
+async function onActivate(plugin: ReactRNPlugin) { 
+  await plugin.app.toast('🟥🟧🟨🟩🟦🟪 Callout Refreshed!!');
+  
+  await plugin.app.registerPowerup(
+    'Callout',
+    CALLOUT_POWERUP,
+    'A Power-up Block for decorating texts',
+    {
+      slots: [
+        {
+          code: 'callout',
+          name: 'Callout',
+          onlyProgrammaticModifying: true,
+          hidden: true,
+        },
+      ],
+    },
+  );
+  await plugin.app.registerCommand({
+    id: "browneyedsoul-Callout",
+    name: "Callout",
+    action: async () => {
+      const rem = await plugin.focus.getFocusedRem();
+      await rem?.addPowerup(CALLOUT_POWERUP);
+    },
+  });
+}
 
 async function onDeactivate(_: ReactRNPlugin) {}
 
